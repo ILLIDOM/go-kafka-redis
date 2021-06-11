62e09912bee8d791f89c6ae0e069093d69b265c0
DILLI <illi.dominique.v@gmail.com>
2021-06-11 11:29:53 +0200

    added writing to redis

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/Shopify/sarama v1.29.0
+	github.com/go-redis/redis/v8 v8.10.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	google.golang.org/grpc v1.38.0 // indirect
 )
--- graph-db-service/graph-db-service.go
+++ graph-db-service/graph-db-service.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 
 	"github.com/Shopify/sarama"
+	"github.com/go-redis/redis/v8"
 )
+
+var ctx = context.Background()
 
 func createConsumer() sarama.Consumer {
 	broker := []string{"localhost:9092"}
@@ -18,7 +23,17 @@ func createConsumer() sarama.Consumer {
 	return consumer
 }
 
+func GetRedisClient() *redis.Client {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+	return rdb
+}
+
 func main() {
+	redisClient := GetRedisClient()
 	consumer := createConsumer()
 	partitionConsumer, err := consumer.ConsumePartition("test", 0, sarama.OffsetNewest)
 	if err != nil {
@@ -30,6 +45,18 @@ func main() {
 			log.Printf("Consumed Message offset %d\n", msg.Offset)
 			log.Printf("Message was: %s", string(msg.Value))
 			// Check if Cache has data else get it from arangodb
+			val, getErr := redisClient.Get(ctx, string(msg.Value)).Result()
+			if getErr == redis.Nil {
+				//key does not exit -> fetch from arango db and write it into cache
+				setErr := redisClient.Set(ctx, string(msg.Value), "", 0).Err()
+				if setErr != nil {
+					panic(setErr)
+				}
+			} else if getErr != nil {
+				panic(getErr)
+			} else {
+				fmt.Println("key", val)
+			}
 		}
 	}
 
